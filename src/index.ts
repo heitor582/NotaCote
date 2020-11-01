9741715c6ef1e9faed932a0682039e3306f31353
Heitor <heitorvieira.pvd@gmail.com>
2020-11-01 11:48:36 -0300

    sort

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import dotenv from "dotenv";
+import fs from "fs";
 
 dotenv.config();
 
@@ -12,13 +13,22 @@ class Main {
   }
   async pegarNota() {
     try {
-      const data = await axios.get(this.url, {
-        auth:{
+      const res = await axios.get(this.url, {
+        auth: {
           username: this.user,
-          password: this.password
-        }
+          password: this.password,
+        },
       });
-      console.log(data.data);
+
+      let disciplinas = res.data.etapas.map((v: any) => {
+        v.disciplinas.sort((a: { id: number; }, b: { id: number; }) => {
+          if (a.id > b.id) return -1;
+          if (a.id < b.id) return 1;
+          return 0;
+        });
+      });
+
+      fs.writeFileSync("../notas.txt", JSON.parse(disciplinas));
     } catch (err) {
       console.log(err);
     }
