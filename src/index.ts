04faf8f1e64b9e67e1bf3c28c4adc3b75ff469ed
Heitor <heitorvieira.pvd@gmail.com>
2020-11-01 12:11:16 -0300

    sort

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import dotenv from "dotenv";
-import fs from "fs";
+import fs, { read } from "fs";
 
 dotenv.config();
 
@@ -20,13 +20,13 @@ class Main {
         },
       });
 
-      let disciplinas = res.data.etapas.map((v: any) => {
+      let disciplinas = res.data.etapas.map((v: any) => (
         v.disciplinas.sort((a: { id: number; }, b: { id: number; }) => {
           if (a.id > b.id) return -1;
           if (a.id < b.id) return 1;
           return 0;
-        });
-      });
+        })
+      ));
 
       fs.writeFileSync("../notas.txt", JSON.parse(disciplinas));
       fs.open('../notas.txt','w', (err,file)=>{
