aba09f1174a5e7e0909da27eae7e9a9ca660813b
Heitor <heitorvieira.pvd@gmail.com>
2020-10-31 21:59:03 -0300

    mudando para https

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
+import https from "https";
 import dotenv from "dotenv";
-import axios from "axios";
+
 const url: string = "https://api.cotemig.com.br/v1/boletim";
 
 dotenv.config();
@@ -7,14 +8,31 @@ dotenv.config();
 const user: string = process.env.USER as string;
 const password: string = process.env.PASSWORD as string;
 
-axios
-  .get(url, {
-    headers: {
-      Authorization: `Basic ${Buffer.from(`${user}:${password}`).toString(
-        "base64"
-      )}`,
-    },
+const options = {
+  host: url,
+  Authorization: `Basic ${Buffer.from(`${user}:${password}`).toString(
+    "base64"
+  )}`,
+};
+
+https
+  .get(options, function (res) {
+    res.setEncoding("utf8");
+    res.on("data", function (data) {
+      if (res.statusCode == 200) {
+        const json = JSON.parse(data);
+        console.log(json);
+      } else {
+        try {
+          var json = JSON.parse(data);
+          console.log("ERRO " + json.erro + ": " + json.detalhes);
+        } catch (e) {
+          console.log("ERRO 500: Erro interno do servidor.");
+        }
+      }
+    });
   })
-  .then((res) => {
-    console.log(res.data);
-  });
+  .on("error", function (e) {
+    console.error(e);
+  })
+  .end();
