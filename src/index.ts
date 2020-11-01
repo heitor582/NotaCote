6a662f752249afa162b1145a44697f875a5c9a09
Heitor <heitorvieira.pvd@gmail.com>
2020-11-01 10:29:31 -0300

    mudando pra classe

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,30 @@
-import https from "https";
+import axios from "axios";
 import dotenv from "dotenv";
 
-const url: string = "https://api.cotemig.com.br/v1/boletim";
+const url: string = "http://api.cotemig.com.br/v1/boletim";
 
 dotenv.config();
 
 const user: string = process.env.USER as string;
 const password: string = process.env.PASSWORD as string;
 
-const options = {
-  host: url,
-  Authorization: `Basic ${Buffer.from(`${user}:${password}`).toString(
-    "base64"
-  )}`,
-};
-
-https
-  .get(options, function (res) {
-    res.setEncoding("utf8");
-    res.on("data", function (data) {
-      if (res.statusCode == 200) {
-        const json = JSON.parse(data);
-        console.log(json);
-      } else {
-        try {
-          var json = JSON.parse(data);
-          console.log("ERRO " + json.erro + ": " + json.detalhes);
-        } catch (e) {
-          console.log("ERRO 500: Erro interno do servidor.");
-        }
-      }
-    });
-  })
-  .on("error", function (e) {
-    console.error(e);
-  })
-  .end();
+class Main {
+  constructor() {
+    this.pegarNota();
+  }
+  async pegarNota() {
+    try{
+      const data = await axios.get(url, {
+        headers: {
+          Authorization: `Basic ${Buffer.from(`${user}:${password}`).toString(
+            "base64"
+          )}`,
+        },
+      });
+      console.log(data.data)
+    }catch(err){
+      console.log(err);
+    }
+  }
+}
+new Main();
