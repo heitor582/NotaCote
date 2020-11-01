45587225d78f1db11236a000a2d0e727423bf4be
Heitor <heitorvieira.pvd@gmail.com>
2020-11-01 11:04:13 -0300

    refatorando

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,25 @@
 import axios from "axios";
 import dotenv from "dotenv";
 
-const url: string = "http://api.cotemig.com.br/v1/boletim";
-
 dotenv.config();
 
-const user: string = process.env.USER as string;
-const password: string = process.env.PASSWORD as string;
-
 class Main {
+  url: string = "http://api.cotemig.com.br/v1/boletim";
+  user: string = process.env.USER as string;
+  password: string = process.env.PASSWORD as string;
   constructor() {
     this.pegarNota();
   }
   async pegarNota() {
-    try{
-      const data = await axios.get(url, {        
-        auth:{
-          username: user,
-          password
-        }
+    try {
+      const data = await axios.get(this.url, {
+        auth: {
+          username: this.user,
+          password: this.password,
+        },
       });
-      console.log(data.data)
-    }catch(err){
+      console.log(data.data);
+    } catch (err) {
       console.log(err);
     }
   }
