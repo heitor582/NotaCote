1ba70acb8c16e826c462484a289cce4ee09ca28f
Heitor <heitorvieira.pvd@gmail.com>
2020-11-05 10:36:16 -0300

    .

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { notStrictEqual } from "assert";
 import axios from "axios";
 import dotenv from "dotenv";
 
@@ -19,16 +20,23 @@ class Main {
         },
       });
 
-      let disciplinas = res.data.etapas.map((v: any) => (
+      let disciplinasArray = res.data.etapas.map((v: any) => (
         v.disciplinas.sort((a: { id: number; }, b: { id: number; }) => {
           if (a.id > b.id) return -1;
           if (a.id < b.id) return 1;
           return 0;
         })
       ));
-      let notas: any = [];
       
-      console.log(disciplinas);
+      let disciplinas:any = [];
+
+      disciplinasArray.map((v:any)=>{
+        v.map((j:any)=>{
+          disciplinas.push(j);
+        })
+      })
+      
+      console.log();
     } catch (err) {
       console.log(err);
     }
